32349f5aaef5eb485e723c8d4d7f73b317cda44c
voidpunk <davide.raucci.k@gmail.com>
2022-01-07 15:08:01 +0100

    increased query functionalities & added SIDER model

--- model.py
+++ model.py
@@ -5,23 +5,48 @@ from torchdrug import data, models, tasks
 from rdkit import Chem
 
 
-def query(key: "str") -> "str":
+def query(key: "str") -> "tuple(str, dict)":
     """
     Query the ChEMBL database for a molecule and return its InChI.
     """
 
     # query the ChEMBL database for the molecule
-    molecule = new_client.molecule
-    molecule_structure = molecule\
-        .filter(molecule_synonyms__molecule_synonym__iexact=key)\
-        .only("molecule_structures")
+    molecule = new_client.molecule\
+        .filter(molecule_synonyms__molecule_synonym__iexact=key)[0]
+        # .only("molecule_structures")[0]
+
+    # print("\n\n", molecule, "\n\n")
 
     # retrieve the InChi
-    inchi = molecule_structure[0]["molecule_structures"]["standard_inchi"]
+    inchi = molecule["molecule_structures"]["standard_inchi"]
     print(inchi)
     # print(type(inchi))
+    first_approval = molecule["first_approval"]
+    print(first_approval)
+    indication_class = molecule["indication_class"]
+    print(indication_class)
+    max_phase = molecule["max_phase"]
+    print(max_phase)
+    natural_product = molecule["natural_product"]
+    print(natural_product)
+    oral = molecule["oral"]
+    print(oral)
+    parenteral = molecule["parenteral"]
+    print(parenteral)
+    topical = molecule["topical"]
+    print(topical)
 
-    return inchi
+    infos = {
+        "first_approval": first_approval,
+        "indication_class": indication_class,
+        "max_phase": max_phase,
+        "natural_product": natural_product,
+        "oral": oral,
+        "parenteral": parenteral,
+        "topical": topical
+    }
+
+    return inchi, infos
 
 
 def construct(inchi: "str") -> "graph":
@@ -41,7 +66,7 @@ def construct(inchi: "str") -> "graph":
     return graph
 
 
-def load():
+def load_clintox():
 
     # define the model
     model = models.GIN(
@@ -55,7 +80,10 @@ def load():
     # define the task
     task = tasks.PropertyPrediction(
         model,
-        task=["FDA_APPROVED", "CT_TOX"],
+        task=[
+            "FDA_APPROVED",
+            "CT_TOX"
+            ],
         criterion="bce",
         metric=(
             # "auprc",
@@ -70,7 +98,64 @@ def load():
     return model, task
 
 
-def predict(graph, model, task) -> "dict":
+def load_sider():
+
+    # define the model
+    model = models.GIN(
+        input_dim=69,
+        hidden_dims=[256, 256, 256, 256],
+        short_cut=True,
+        batch_norm=True,
+        concat_hidden=True
+        )
+
+    # define the task"auroc"
+    task = tasks.PropertyPrediction(
+        model,
+        task=[
+            "Hepatobiliary disorders",
+            "Metabolism and nutrition disorders",
+            "Product issues",
+            "Eye disorders",
+            "Investigations",
+            "Musculoskeletal and connective tissue disorders",
+            "Gastrointestinal disorders",
+            "Social circumstances",
+            "Immune system disorders",
+            "Reproductive system and breast disorders",
+            "Neoplasms benign, malignant and unspecified (incl cysts and polyps)",
+            "General disorders and administration site conditions",
+            "Endocrine disorders",
+            "Surgical and medical procedures",
+            "Vascular disorders",
+            "Blood and lymphatic system disorders",
+            "Skin and subcutaneous tissue disorders",
+            "Congenital, familial and genetic disorders",
+            "Infections and infestations",
+            "Respiratory, thoracic and mediastinal disorders",
+            "Psychiatric disorders",
+            "Renal and urinary disorders",
+            "Pregnancy, puerperium and perinatal conditions",
+            "Ear and labyrinth disorders",
+            "Cardiac disorders",
+            "Nervous system disorders",
+            "Injury, poisoning and procedural complications",
+    ],
+        criterion="bce",
+        metric=(
+            # "auprc",
+            "auroc"
+            )
+        )
+
+    # load the weights and task settings
+    checkpoint = torch.load("models/sider/sider_model.pth")["model"]
+    task.load_state_dict(checkpoint, strict=False)
+
+    return model, task
+
+
+def predict(graph, model, task, dataset) -> "dict":
 
     # predict the ClinTox features of the retrieved molecule
     with torch.no_grad():
@@ -84,21 +169,66 @@ def predict(graph, model, task) -> "dict":
         # print(pred.shape)
         # print(pred[0][0].item())
         # print(pred[0][1].item())
-        pred = {
-            "FDA approved": round(pred[0][0].item()*100, 2),
-            "toxic": round(pred[0][1].item()*100, 2)
-        }
+        if dataset == "clintox":
+            pred = {
+                "FDA approval": round(pred[0][0].item()*100, 2),
+                "toxicity": round(pred[0][1].item()*100, 2)
+            }
+        elif dataset == "sider":
+            pred = {
+            "Hepatobiliary disorders": round(pred[0][0].item()*100, 2),
+            "Metabolism & nutrition disorders": round(pred[0][1].item()*100, 2),
+            "Product issues": round(pred[0][2].item()*100, 2),
+            "Eye disorders": round(pred[0][3].item()*100, 2),
+            "Investigations": round(pred[0][4].item()*100, 2),
+            "Musculoskeletal & connective tissue disorders": round(pred[0][5].item()*100, 2),
+            "Gastrointestinal disorders": round(pred[0][6].item()*100, 2),
+            "Social circumstances": round(pred[0][7].item()*100, 2),
+            "Immune system disorders": round(pred[0][8].item()*100, 2),
+            "Reproductive system & breast disorders": round(pred[0][9].item()*100, 2),
+            "Neoplasms benign, malignant & unspecified": round(pred[0][10].item()*100, 2),
+            "General disorders & administration site conditions": round(pred[0][11].item()*100, 2),
+            "Endocrine disorders": round(pred[0][12].item()*100, 2),
+            "Surgical & medical procedures": round(pred[0][13].item()*100, 2),
+            "Vascular disorders": round(pred[0][14].item()*100, 2),
+            "Blood & lymphatic system disorders": round(pred[0][15].item()*100, 2),
+            "Skin & subcutaneous tissue disorders": round(pred[0][16].item()*100, 2),
+            "Congenital, familial & genetic disorders": round(pred[0][17].item()*100, 2),
+            "Infections & infestations": round(pred[0][18].item()*100, 2),
+            "Respiratory, thoracic & mediastinal disorders": round(pred[0][19].item()*100, 2),
+            "Psychiatric disorders": round(pred[0][20].item()*100, 2),
+            "Renal & urinary disorders": round(pred[0][21].item()*100, 2),
+            "Pregnancy, puerperium & perinatal conditions": round(pred[0][22].item()*100, 2),
+            "Ear & labyrinth disorders": round(pred[0][23].item()*100, 2),
+            "Cardiac disorders": round(pred[0][24].item()*100, 2),
+            "Nervous system disorders": round(pred[0][25].item()*100, 2),
+            "Injury, poisoning & procedural complications": round(pred[0][26].item()*100, 2)
+            }
 
         return pred
 
 
+eval1 = False
+eval2 = True
 
 if __name__ == "__main__":
-    inchi = query("aspirin")
-    graph = construct(inchi)
-    model, task = load()
-    pred = predict(graph, model, task)
-    print("\n", pred)
+    if eval1 == True:
+        inchi, _ = query("aspirin")
+        graph = construct(inchi)
+        model, task = load_clintox()
+        pred = predict(graph, model, task, "clintox")
+        print("\nClinTox:", pred)
+        model, task = load_sider()
+        pred = predict(graph, model, task, "sider")
+        print("\nSIDER:", pred)
+    if eval2 == True:
+        from torchdrug import datasets
+        dataset = datasets.SIDER("./data/sider/")
+        model, task = load_sider()
+        batch = data.graph_collate(dataset[:10])
+        pred = task.predict(batch)
+        print(batch)
+        print(pred)
 else:
     pass
 
